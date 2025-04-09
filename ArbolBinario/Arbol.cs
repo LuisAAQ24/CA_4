3e1bda3ac74deef8fb3d249d57c20bcd39b5e824
Luis Aguero <l.aguero.2@estudiantec.cr>
2025-04-08 19:56:28 -0600

    archivos finales

---
+++ ArbolBinario/Arbol.cs
@@ -0,0 +1,155 @@
+﻿using System;
+
+namespace Arbol
+{
+    // Nodo del árbol
+    public class NodoArbol
+    {
+        public int Key;
+        public NodoArbol Left, Right;
+
+        public NodoArbol(int item)
+        {
+            Key = item;
+            Left = Right = null;
+        }
+    }
+
+    //Arbol
+    public class ArbolBinario
+    {
+        private NodoArbol root;
+
+        public ArbolBinario()
+        {
+            root = null;
+        }
+
+        // Búsqueda pública
+        public bool Buscar(int key)
+        {
+            return BuscarRecursivo(root, key);
+        }
+
+        private bool BuscarRecursivo(NodoArbol root, int key)
+        {
+            if (root == null) return false;
+            if (root.Key == key) return true;
+            else if (key > root.Key) return BuscarRecursivo(root.Right, key);
+            else return BuscarRecursivo(root.Left, key);
+        }
+
+        // Inserción pública
+        public void Insert(int key)
+        {
+            root = InsertRecursive(root, key);
+        }
+
+        private NodoArbol InsertRecursive(NodoArbol root, int key)
+        {
+            if (root == null)
+            {
+                return new NodoArbol(key);
+            }
+
+            if (key < root.Key)
+                root.Left = InsertRecursive(root.Left, key);
+            else if (key > root.Key)
+                root.Right = InsertRecursive(root.Right, key);
+
+            return root;
+        }
+
+        // Eliminación pública
+        public void Delete(int key)
+        {
+            root = DeleteRecursive(root, key);
+        }
+
+        private NodoArbol DeleteRecursive(NodoArbol root, int key)
+        {
+            if (root == null) return root;
+
+            if (key < root.Key)
+                root.Left = DeleteRecursive(root.Left, key);
+            else if (key > root.Key)
+                root.Right = DeleteRecursive(root.Right, key);
+            else
+            {
+                //sin hijos
+                if (root.Left == null)
+                    return root.Right;
+                else if (root.Right == null)
+                    return root.Left;
+
+                //con hijos
+                root.Key = MinValue(root.Right);
+                root.Right = DeleteRecursive(root.Right, root.Key);
+            }
+
+            return root;
+        }
+
+        private int MinValue(NodoArbol root)
+        {
+            int min = root.Key;
+            while (root.Left != null)
+            {
+                min = root.Left.Key;
+                root = root.Left;
+            }
+            return min;
+        }
+
+        //inorden
+        public void InOrder()
+        {
+            InOrderRecursivo(root);
+            Console.WriteLine();
+        }
+
+        private void InOrderRecursivo(NodoArbol root)
+        {
+            if (root != null)
+            {
+                InOrderRecursivo(root.Left);
+                Console.Write(root.Key + " ");
+                InOrderRecursivo(root.Right);
+            }
+        }
+
+        // Recorrido preorden
+        public void PreOrder()
+        {
+            PreOrderRecursivo(root);
+            Console.WriteLine();
+        }
+
+        private void PreOrderRecursivo(NodoArbol root)
+        {
+            if (root != null)
+            {
+                Console.Write(root.Key + " ");
+                PreOrderRecursivo(root.Left);
+                PreOrderRecursivo(root.Right);
+            }
+        }
+
+        // Recorrido postorden
+        public void PostOrder()
+        {
+            PostOrderRecursivo(root);
+            Console.WriteLine();
+        }
+
+        private void PostOrderRecursivo(NodoArbol root)
+        {
+            if (root != null)
+            {
+                PostOrderRecursivo(root.Left);
+                PostOrderRecursivo(root.Right);
+                Console.Write(root.Key + " ");
+            }
+        }
+    }
+}
--- TestArbolBinario/Test1.cs
+++ TestArbolBinario/Test1.cs
@@ -1,11 +1,77 @@
-﻿namespace TestArbolBinario
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Arbol;
+
+namespace TestArbolBinario
 {
     [TestClass]
-    public sealed class Test1
+    public class ArbolBinarioTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void InsertarElemento()
         {
+            var bst = new ArbolBinario();
+            bst.Insert(8);
+            bst.Insert(3);
+            bst.Insert(10);
+            bst.Insert(1);
+            bst.Insert(6);
+
+            Assert.IsTrue(bst.Buscar(8));
+            Assert.IsTrue(bst.Buscar(3));
+            Assert.IsTrue(bst.Buscar(10));
+            Assert.IsTrue(bst.Buscar(1));
+            Assert.IsTrue(bst.Buscar(6));
+            Assert.IsFalse(bst.Buscar(99));
+        }
+
+        [TestMethod]
+        public void EliminarNodo()
+        {
+            var bst = new ArbolBinario();
+            bst.Insert(5);
+            bst.Insert(3);
+            bst.Insert(7);
+
+            bst.Delete(3); // Nodo
+            Assert.IsFalse(bst.Buscar(3));
+        }
+
+        [TestMethod]
+        public void ReemplazarUnHijo()
+        {
+            var bst = new ArbolBinario();
+            bst.Insert(5);
+            bst.Insert(3);
+            bst.Insert(1); // hijo izquierdo de 3
+
+            bst.Delete(3);
+            Assert.IsFalse(bst.Buscar(3));
+            Assert.IsTrue(bst.Buscar(1));
+        }
+
+        [TestMethod]
+        public void ReemplazarDosHijos()
+        {
+            var bst = new ArbolBinario();
+            bst.Insert(8);
+            bst.Insert(3);
+            bst.Insert(10);
+            bst.Insert(1);
+            bst.Insert(6);
+            bst.Insert(4);
+            bst.Insert(7);
+
+            bst.Delete(3); // tiene dos hijos
+
+            Assert.IsFalse(bst.Buscar(3));
+            Assert.IsTrue(bst.Buscar(4)); // El sucesor debería seguir estando
+        }
+
+        [TestMethod]
+        public void BuscarVacio()
+        {
+            var bst = new ArbolBinario();
+            Assert.IsFalse(bst.Buscar(10));
         }
     }
 }
